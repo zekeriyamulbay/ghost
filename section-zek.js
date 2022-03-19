dfd02cf46facae030a62d76f7a08dbe54b89a522
Zekeriya Mulbay <67113841+zekeriyamulbay@users.noreply.github.com>
2022-03-19 22:31:59 +0000

    Update section-zek.js

--- section-zek.js
+++ section-zek.js
@@ -10,5 +10,5 @@ function addSectionList(el) {
         document.getElementsByClassName("section-list")[0].innerHTML += ('<li class="section-list-item" style="background-color: ' + el.parentElement.parentElement.parentElement.style.backgroundColor + '"><a href="#' + el.id + '"><img class="section-img" src="' + el.parentElement.parentElement.nextElementSibling.children[1].firstElementChild.src + '" width="200" height="200" decoding="async" loading="lazy" data-aspect-ratio="1"></img></a><div class="section-text"><a href="#' + el.id + '" style="color:' + el.parentElement.parentElement.children[0].children[0].style.color + '">' + el.parentElement.parentElement.children[0].children[0].innerText + '</a></div><p class="section-title"><a href="#' + el.id + '" style="color:' + el.style.color + '">' + el.textContent + '</a></p></li>');
 }
 window.addEventListener('load', function() {
-    document.querySelectorAll('.single-content h2').forEach(el => addSectionList(el));
+    document.querySelectorAll('.chapter-title h2').forEach(el => addSectionList(el));
 })
